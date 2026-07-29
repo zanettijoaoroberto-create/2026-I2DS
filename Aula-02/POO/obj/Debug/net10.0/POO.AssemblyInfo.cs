455d57793b9c3cfee9036ceea5bd5dbd5696473f
zanettijoaoroberto-create <zanettijoaoroberto@gmail.com>
2026-07-29 16:40:11 -0300

    Aula 02 - Classe e objeto

--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e42ad50952c1ddf797756b88d15993e754f7be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50d1e0b5dc47b6f41b963b528a14ed8926b69c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
